ed3cefd4ed6d0f5c15520b85bad724a611ab1a9d
Mahdi Aghaali <mahdi.aghaali@yahoo.com>
2023-02-09 21:40:13 +0330

    Update routes and application rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
-Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# frozen_string_literal: true
 
-  resources :recipes, only: [:index, :show, :new, :create, :destroy]
+Rails.application.routes.draw do
+  resources :foods
+  devise_for :users
+  resources :users
+  resources :recipes, only: %i[index show new create destroy] do
+    resources :recipe_foods, only: %i[create destroy new]
+  end
+
+  root to: 'foods#index'
+
+  get '/public_recipes', to: 'recipes#public_recipes'
+  get '/shopping_list', to: 'foods#shopping_list'
+  post '/public_toggle/:id', to: 'recipes#public_toggle'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "users#index"
 end
